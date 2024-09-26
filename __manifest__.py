57a1c66db2c1c551a1c0b4b1ad5775a6b933f57f
Ajay Rawat <158762489+Ajayrawat17@users.noreply.github.com>
2024-09-26 21:21:06 +0530

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'accounting',
-    'version': '2.0',
+    'version': '1.0',
     # 'category': 'Human Resources/Employees',
     'sequence': 100,
     # 'summary': 'Centralize employee information',
